c5982d0ff259b56043570a5f05625e9556874507
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-08-05 14:08:17 +0200

    v0.3.4

--- src/model/mediasoup/MediasoupDevice.ts
+++
@@ -1,7 +0,0 @@
-import { BrowserDevice } from '../browser'
-
-interface MediasoupDevice<IdType = string> extends Omit<BrowserDevice<IdType>, 'type'> {
-    type: 'mediasoup'
-    canMediasoup: true
-}
-export default MediasoupDevice
--- src/model/mediasoup/index.ts
+++
@@ -1,6 +0,0 @@
-import MediasoupAudioTrack from './MediasoupAudioTrack'
-import MediasoupVideoTrack from './MediasoupVideoTrack'
-import MediasoupStage from './MediasoupStage'
-import MediasoupDevice from './MediasoupDevice'
-
-export type { MediasoupDevice, MediasoupAudioTrack, MediasoupVideoTrack, MediasoupStage }
