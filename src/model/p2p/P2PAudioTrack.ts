89405e357f11fec372ead2b331453675d73a97a2
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-08-05 13:59:13 +0200

    v0.3.2

--- src/model/p2p/P2PAudioTrack.ts
+++
@@ -1,9 +0,0 @@
-import AudioTrack from '../AudioTrack'
-
-interface P2PAudioTrack<IdType = string> extends AudioTrack<IdType> {
-    type: 'p2p'
-
-    offer: RTCSessionDescriptionInit
-}
-
-export default P2PAudioTrack
--- src/model/p2p/P2PVideoTrack.ts
+++
@@ -1,9 +0,0 @@
-import VideoTrack from '../VideoTrack'
-
-interface P2PVideoTrack<IdType = string> extends VideoTrack<IdType> {
-    type: 'p2p'
-
-    offer: RTCSessionDescriptionInit
-}
-
-export default P2PVideoTrack
